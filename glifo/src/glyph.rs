460b171b13bb0838e47a892e8c6cb65d2fd2b4aa
James Lelong <james.lelong@canva.com>
2026-04-29 10:19:37 +1000

    drop skrifa version

--- glifo/src/glyph.rs
+++ glifo/src/glyph.rs
@@ -1826,7 +1826,6 @@ pub struct HintCache {
     // internal memory when reconfigured for the same format.
     glyf_entries: Vec<HintEntry>,
     cff_entries: Vec<HintEntry>,
-    varc_entries: Vec<HintEntry>,
     serial: u64,
 }
 
@@ -1835,7 +1834,6 @@ impl Debug for HintCache {
         f.debug_struct("HintCache")
             .field("glyf_entries", &self.glyf_entries.len())
             .field("cff_entries", &self.cff_entries.len())
-            .field("varc_entries", &self.varc_entries.len())
             .field("serial", &self.serial)
             .finish()
     }
@@ -1847,7 +1845,6 @@ impl HintCache {
         let entries = match key.outlines.format()? {
             OutlineGlyphFormat::Glyf => &mut self.glyf_entries,
             OutlineGlyphFormat::Cff | OutlineGlyphFormat::Cff2 => &mut self.cff_entries,
-            OutlineGlyphFormat::Varc => &mut self.varc_entries,
         };
         let (entry_ix, is_current) = find_hint_entry(entries, key)?;
         let entry = entries.get_mut(entry_ix)?;
@@ -1873,7 +1870,6 @@ impl HintCache {
     pub fn clear(&mut self) {
         self.glyf_entries.clear();
         self.cff_entries.clear();
-        self.varc_entries.clear();
         self.serial = 0;
     }
 }
